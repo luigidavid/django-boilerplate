cbadf4c23e44d381fe6b36ec644fa4655f0ba4ce
Salvador Nicolas <snicoper@gmail.com>
2017-03-21 18:23:43 +0100

    Añadido # flake8: noqa

--- console.py
+++ console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# flake8: noqa
 # Para pruebas rapidas, similar a ./manage.py shell
 
 import os
@@ -7,7 +7,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.config.settings.local')
 
 import django
 django.setup()
-####################################################################################################
 ####################################################################################################
 from django.conf import settings
 
--- src/config/settings/test.py
+++ src/config/settings/test.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 from .base import *
 
 # Añadir ~/tests a PYTHONPATH
